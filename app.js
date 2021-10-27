8fd7aa5a48a9d402e9a47d8a795984f944463721
Tayyab Mehmood <88029493+tayyab-nbs@users.noreply.github.com>
2021-10-27 19:55:03 +0500

    minor changes

--- app.js
+++ app.js
@@ -13,9 +13,12 @@ router.get("/about", function (req, res) {
   res.sendFile(path.join(__dirname + "/about.html"));
 });
 
-router.get("/receiveWebhook", function (req, res) {
-  console.log("YAHOO WEEBHOOK RECEIVED", res);
-});
+router.patch(
+  "https://services.sheerid.com/rest/v2/program/6177e7c5b761aa366d5a96d5/webhook",
+  function (req, res) {
+    console.log("YAHOO WEEBHOOK RECEIVED", res);
+  }
+);
 
 router.get("/getData", (req, response) => {
   const token = "7a109c01af2a4ed3b161f26ce4ef6d2b";
@@ -28,7 +31,7 @@ router.get("/getData", (req, response) => {
   };
 
   const bodyParameters = {
-    callbackUri: "https://webhook007755-app.herokuapp.com/receiveWebhook",
+    callbackUri: "https://webhook007755-app.herokuapp.com",
   };
   axios
     .post(
