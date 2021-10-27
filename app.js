cbda3ebedc4731414772b0a95c934380e21f98a9
Tayyab Mehmood <88029493+tayyab-nbs@users.noreply.github.com>
2021-10-27 17:03:48 +0500

    Fixed port issue

--- app.js
+++ app.js
@@ -36,6 +36,6 @@ router.get("/sitemap", function (req, res) {
 
 //add the router
 app.use("/", router);
-app.listen(process.env.port || 3000, () => {
-  console.log("Running at Port: 3000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Running at Port: 5000");
 });
